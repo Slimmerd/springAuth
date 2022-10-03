72a72bf44b83ed2dee4160a21271358beefaa439
Daniil Silin <daniil_silin@mail.ru>
2022-10-03 19:47:19 +0100

    Initial commit

---
+++ src/test/java/ru/test/authSpring/AuthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test.authSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
